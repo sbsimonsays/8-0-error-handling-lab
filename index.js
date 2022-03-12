a4280c7f0262938d46aef8f717df860a6648267d
Spencer Simon <spencersimon@pursuit.org>
2022-03-12 17:11:32 -0500

    Error Handling

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ const exampleProducts = [
 */
 function getCartTotal(cart) {
   let result = 0;
+  if (!cart || cart.length === 0){
+    throw "ERROR: Cart is empty!";
+  }
   for (let product of cart) {
     result += product.priceInCents;
   }
@@ -35,25 +38,84 @@ function getCartTotal(cart) {
   - Either `min` or `max` is less than `0`.
   - Any of the products in the `products` array does not have a `priceInCents` key.
 */
+// function filterProductsByPriceRange(products, min, max) {
+//   const result = [];
+//   if (products.length == 0) {
+//     throw "Error: No products found!";
+//   }
+//   if (typeof min !== "number" || typeof max !== "number") {
+//     throw "Error: Either min or max is not a number!";
+//   }
+//   if (max === 0) {
+//     throw "Error: max value is equal to zero!";
+//   }
+//   if (min > max) {
+//     throw "Error: min is greater than max!";
+//   }
+//   if (max < 0 || min < 0) {
+//     throw "Error: Either min or max value is less than zero!";
+//   }
+// for (let product of products) {
+//   if (!product.priceInCents) {
+//     throw "Error: priceInCents key is not found!";
+//   }
+//   if (product.priceInCents >= min && product.priceInCents <= max) {
+//      result.push(product);
+//   }}
+//   return result;
+// }
+
 function filterProductsByPriceRange(products, min, max) {
+  switch (true){
+  case !products.length:
+    throw 'ERROR: No products found!';
+    break;
+  case typeof min !== 'number' || typeof max !== 'number':
+    throw 'ERROR: Either min or max is not a number!';
+    break;
+  case max === 0:
+    throw 'ERROR: max value is equal to zero!';
+    break;
+  case min > max:
+    throw 'ERROR: min is greater than max!';
+    break;
+  case min < 0 || max < 0:
+    throw 'ERROR: Either min or max value is less than zero!'
+    break; 
+  };
   const result = [];
   for (let product of products) {
+    if (!product.priceInCents) {
+      throw "ERROR: priceInCents key is not found!";
+    }
     if (product.priceInCents >= min && product.priceInCents <= max) {
       result.push(product);
-    }
-  }
-  return result;
+    }}
+    return result;
 }
 
 /*
   If any errors occur in this function, it should return `0`.
 */
 function getTotalOfAllProductsByPriceRange(products, min, max) {
-  const filteredProducts = filterProductsByPriceRange(products, min, max);
-  const total = getCartTotal(filteredProducts);
+  try {
+    const filteredProducts = filterProductsByPriceRange(products, min, max);
+    const total = getCartTotal(filteredProducts);
+    return total;
+      }
+  catch (error) {
+  return 0;
+}}
 
-  return total;
-}
+// const getTotalOfAllProductsByPriceRange = (products, min, max) => {
+// try {
+//   const filteredProducts = filterProductsByPriceRange(products, min, max);
+//   const total = getCartTotal(filteredProducts);
+//   return total;
+// }
+// catch (err) {
+//   return 0;
+// }}
 
 module.exports = {
   getCartTotal,
